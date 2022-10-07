c2b2b106da9eeb33bd42f3ff2a46e337a6ef3b1f
m1rade <forx98@gmail.com>
2022-10-07 15:10:38 +0300

    Fixed Project component's css

--- src/components/projects/Project/Project.js
+++ src/components/projects/Project/Project.js
@@ -5,8 +5,7 @@ import sCard from "../../../common/styles/Card.module.css";
 const Project = (props) => {
     return (
         <div className={sCard.container}>
-            <div className={s.picture}>
-                <div>picture</div>
+            <div className={s.pictureContainer}>
                 <button>See</button>
             </div>
             <h3>{props.title}</h3>
