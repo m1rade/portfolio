8498cdef6f2af3b82b9aea30fc203933c1c735c1
m1rade <forx98@gmail.com>
2023-05-30 01:03:27 +0300

    Add my tech skills

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -4,13 +4,7 @@ import sSection from "common/styles/section.module.css";
 import sWrapper from "common/styles/wrapperForCards.module.css";
 import { Skill } from "components/skills/skill";
 import React from "react";
-import { SiJavascript } from "react-icons/si";
-import { SiRedux } from "react-icons/si";
-import { SiReact } from "react-icons/si";
-import { SiTypescript } from "react-icons/si";
-import { SiHtml5 } from "react-icons/si";
-import { SiCss3 } from "react-icons/si";
-import s from "./Skills.module.scss";
+import { SiCss3, SiHtml5, SiJavascript, SiMui, SiReact, SiRedux, SiTypescript } from "react-icons/si";
 
 export const Skills = () => {
     return (
@@ -19,23 +13,26 @@ export const Skills = () => {
                 <Subtitle subtitle="My Skills" />
                 <Title title="Tech" title2="Stack" />
                 <div className={sWrapper.inner}>
-                    <Skill title={"JavaScript"}>
+                    <Skill title="JavaScript">
                         <SiJavascript />
                     </Skill>
-                    <Skill title={"HTML"}>
+                    <Skill title="HTML">
                         <SiHtml5 />
                     </Skill>
                     <Skill title="CSS">
                         <SiCss3 />
                     </Skill>
-                    <Skill title={"React"}>
+                    <Skill title="React">
                         <SiReact />
                     </Skill>
-                    <Skill title={"Redux"}>
+                    <Skill title="Redux/Toolkit">
                         <SiRedux />
                     </Skill>
-                    <Skill title={"Typescript"}>
+                    <Skill title="Typescript">
                         <SiTypescript />
+                    </Skill>
+                    <Skill title="Material UI">
+                        <SiMui />
                     </Skill>
                 </div>
             </div>
