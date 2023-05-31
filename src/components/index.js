41fcdea48c8d9767c58fd6a9c40e7c0a92996f62
m1rade <forx98@gmail.com>
2023-05-31 03:46:48 +0300

    Remove header

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { Contacts } from "./contacts/Contacts";
 export { Footer } from "./footer/Footer";
-export { Header } from "./header/Header";
 export { Nav } from "./nav/Nav";
 export { Main } from "./main/Main";
 export { Projects } from "./projects/Projects";
