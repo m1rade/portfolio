e95a0c6e1ebaceebb55196ae0a09b01492b1654a
m1rade <forx98@gmail.com>
2023-06-01 06:02:29 +0300

    Add icon for links

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,16 @@
 import { Button } from "common/components";
+import { navigation } from "common/routes";
 import React from "react";
+import { IconContext } from "react-icons";
+import { AiFillGithub, AiOutlineInstagram } from "react-icons/ai";
 import { HiOutlineEnvelope } from "react-icons/hi2";
+import { SiCodewars, SiSpotify } from "react-icons/si";
 import photo from "../../assets/image/me.jpg";
 import s from "./Sidebar.module.scss";
-import { navigation } from "common/routes";
 
 export const Sidebar = () => {
+    const styledIcon = { className: s.icon, size: "1.2em" };
+
     return (
         <div className={s.container}>
             <div className={s.personalPhoto}>
@@ -14,19 +19,45 @@ export const Sidebar = () => {
             <div className={s.info}>
                 <h2>Golysheva Laura</h2>
                 <h2>Based in Saint-Petersburg</h2>
-                <p className={s.copyright}>© 2022, All Rights Reserved</p>
+                <p className={s.copyright}>© 2023, All Rights Reserved</p>
             </div>
             <div className={s.social}>
                 <ul className={s.links}>
-                    <li>Github</li>
-                    <li>Instagram</li>
-                    <li>Spotify</li>
-                    <li>Codewars</li>
+                    <li>
+                        <a href="https://github.com/m1rade" target="_blank" rel="noreferrer">
+                            <IconContext.Provider value={styledIcon}>
+                                <AiFillGithub />
+                            </IconContext.Provider>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://instagram.com/arctic_cola" target="_blank" rel="noreferrer">
+                            <IconContext.Provider value={styledIcon}>
+                                <AiOutlineInstagram />
+                            </IconContext.Provider>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://spotify.com/" target="_blank" rel="noreferrer">
+                            <IconContext.Provider value={styledIcon}>
+                                <SiSpotify />
+                            </IconContext.Provider>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.codewars.com/users/m1rade" target="_blank" rel="noreferrer">
+                            <IconContext.Provider value={styledIcon}>
+                                <SiCodewars />
+                            </IconContext.Provider>
+                        </a>
+                    </li>
                 </ul>
             </div>
-            <Button className={s.buttonWrapper} icon={<HiOutlineEnvelope />}>
-                <a className={s.buttonLink} href={"#" + navigation.contacts}>Hire me!</a>
-            </Button>
+            <a className={s.buttonLink} href={"#" + navigation.contacts}>
+                <Button className={s.buttonWrapper} icon={<HiOutlineEnvelope />}>
+                    Hire me!
+                </Button>
+            </a>
         </div>
     );
 };
