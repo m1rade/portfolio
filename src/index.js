e7adc803309835a5b1a7c626d60664036a8b5368
m1rade <forx98@gmail.com>
2023-06-06 18:10:20 +0300

    Remove unnecessary

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { App } from "app";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
-import reportWebVitals from "./reportWebVitals";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -14,8 +13,3 @@ root.render(
         </BrowserRouter>
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
