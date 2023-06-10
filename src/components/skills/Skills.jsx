8c566dc7d0fa167f16002243719b617f87c774ae
m1rade <forx98@gmail.com>
2023-06-10 05:48:51 +0300

    Add another skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -3,7 +3,22 @@ import { navigation } from "common/routes";
 import { Skill } from "components/skills/skill";
 import React from "react";
 import { FaGraduationCap } from "react-icons/fa";
-import { SiCss3, SiHtml5, SiJavascript, SiMui, SiReact, SiRedux, SiTypescript } from "react-icons/si";
+import {
+    SiAxios,
+    SiCss3,
+    SiGit,
+    SiHtml5,
+    SiJavascript,
+    SiJest,
+    SiMui,
+    SiPostman,
+    SiReact,
+    SiReactrouter,
+    SiRedux,
+    SiSass,
+    SiStorybook,
+    SiTypescript,
+} from "react-icons/si";
 import s from "./Skills.module.scss";
 
 export const Skills = () => {
@@ -13,26 +28,47 @@ export const Skills = () => {
                 <Subtitle subtitle="My Skills" icon={<FaGraduationCap />} />
                 <Title title="Tech" title2="Stack" />
                 <div className={s.innerContent}>
-                    <Skill title="JavaScript" x={"-180px"} y={"0px"}>
+                    <Skill title="JavaScript" x="-180px" y="0px">
                         <SiJavascript />
                     </Skill>
-                    <Skill title="HTML" x={"0px"} y={"180px"}>
+                    <Skill title="HTML" x="0px" y="180px">
                         <SiHtml5 />
                     </Skill>
-                    <Skill title="CSS" x={"180px"} y={"0px"}>
+                    <Skill title="CSS" x="180px" y="0px">
                         <SiCss3 />
                     </Skill>
-                    <Skill title="React" x={"-180px"} y={"0px"}>
+                    <Skill title="React" x="-180px" y="0px">
                         <SiReact />
                     </Skill>
-                    <Skill title="Redux/Toolkit" x={"0px"} y={"-180px"}>
+                    <Skill title="React-router-dom" x="0px" y="180px">
+                        <SiReactrouter />
+                    </Skill>
+                    <Skill title="Redux/Toolkit" x="0px" y="-180px">
                         <SiRedux />
                     </Skill>
-                    <Skill title="Typescript" x={"180px"} y={"0px"}>
+                    <Skill title="Typescript" x="180px" y="0px">
                         <SiTypescript />
                     </Skill>
-                    <Skill title="Material UI" x={"0px"} y={"180px"}>
+                    <Skill title="Material UI" x="0px" y="180px">
                         <SiMui />
+                    </Skill>
+                    <Skill title="Jest" x="-180px" y="0px">
+                        <SiJest />
+                    </Skill>
+                    <Skill title="Git" x="0px" y="180px">
+                        <SiGit />
+                    </Skill>
+                    <Skill title="Storybook" x="-180px" y="0px">
+                        <SiStorybook />
+                    </Skill>
+                    <Skill title="Postman" x="180px" y="0px">
+                        <SiPostman />
+                    </Skill>
+                    <Skill title="Axios" x="0px" y="-180px">
+                        <SiAxios />
+                    </Skill>
+                    <Skill title="Sass" x="0px" y="180px">
+                        <SiSass />
                     </Skill>
                 </div>
             </div>
